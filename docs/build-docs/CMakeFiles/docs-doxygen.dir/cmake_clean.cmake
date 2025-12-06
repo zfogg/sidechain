7ac90f0366bcd8ab57502a4aeb31cc8bc7ccb589
Zachary Fogg <me@zfo.gg>
2025-12-05 22:32:46 -0500

    removed some accidentally committed crap

--- docs/build-docs/CMakeFiles/docs-doxygen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/docs-doxygen"
-  "doxygen/xml/index.xml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/docs-doxygen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- docs/build-docs/CMakeFiles/docs-open.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/docs-open"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/docs-open.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
