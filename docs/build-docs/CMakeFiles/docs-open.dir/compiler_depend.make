4e38e489fad33a24fa3e78765a5f7708f577b88c
Zachary Fogg <me@zfo.gg>
2025-12-05 22:32:46 -0500

    removed some accidentally committed crap

--- docs/build-docs/CMakeFiles/docs-open.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for docs-open.
-# This may be replaced when dependencies are built.
--- docs/build-docs/CMakeFiles/docs-sphinx.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/docs-sphinx"
-  "html/index.html"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/docs-sphinx.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
